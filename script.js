e304a00075ce8e259e10cdd136100f3d42244de1
Simon Ostermann <simon.ostermann@dfki.de>
2024-12-27 14:28:09 +0100

    fix line endings

--- script.js
+++ script.js
@@ -44,7 +44,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function parseTSV(data) {
     const lines = data.trim().split("\n");
-    const headers = lines.shift().split("\t");
+    const headers_orig = lines.shift().split("\t");
+    const headers = list.map(item => item.replace(/^\s+|\s+$/g, ''));
     console.warn(headers);
     return lines.map((line) => {
         const values = line.split("\t");
