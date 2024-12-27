6e9c4348dea8109aa1be98d15526a19f4b36fffc
Simon Ostermann <simon.ostermann@dfki.de>
2024-12-27 14:08:03 +0100

    Bug fix

--- script.js
+++ script.js
@@ -21,11 +21,12 @@ document.addEventListener("DOMContentLoaded", () => {
                 grouped[year].forEach((pub) => {
                     const li = document.createElement("li");
                     li.classList.add("d-flex", "align-items-center", "p-3", "bg-light", "rounded", "mb-3");
+                    const authors = pub.Authors.replace("Simon Ostermann", "<strong>Simon Ostermann</strong>")
                     li.innerHTML = `
                         <img src="${pub.Image}" alt="${pub.Topic}" class="rounded me-3" style="width: 60px; height: 60px;">
                         <div>
                             <p class="mb-1" style="color: #1a1a1a"><strong>${pub.Title}</strong> - <i>${pub.Source}</i> - <strong><a href="${pub.Link}">paper link</a></strong></p>
-                            <p class="mb-0" style="color: #1a1a1a"><i>${pub.Authors.replace("Simon Ostermann", "<strong>Simon Ostermann</strong>")}</i></p>
+                            <p class="mb-0" style="color: #1a1a1a"><i>${authors}</i></p>
                             <p class="mb-0" style="color: #1a1a1a">${pub.Topic}</p>
                         </div>
                     `;
